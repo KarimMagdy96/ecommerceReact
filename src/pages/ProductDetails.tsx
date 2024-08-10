33276f51030d11ad64d71a4316fcdfd4df3c3080
KarimMagdy96 <karimmagdy096@gmail.com>
2024-08-10 19:11:03 +0300

    up

--- src/pages/ProductDetails.tsx
+++ src/pages/ProductDetails.tsx
@@ -19,12 +19,12 @@ const ProductDetails = () => {
   const { title, price, description, image } = product;
 
   return (
-    <section className=" pt-32 mt-3 pb-12 lg:py-32 min-h-fit flex items-center justify-center">
+    <section className=" pt-12 mt-3 pb-12 lg:py-32  h-screen items-center justify-center">
       <div className="container max-auto">
         <div className=" flex gap-x-16 gap-y-3 flex-col lg:flex-row items-center justify-center">
           <div className=" border p-4  ">
             <img
-              className="   max-w-[200px] lg:max-w-sm"
+              className="  max-h-96   max-w-[150px] lg:max-w-sm"
               src={image}
               alt={title}
             />
