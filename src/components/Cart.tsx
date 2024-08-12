8dc831dac8ef0327c5803f2b1c49acd81272230b
KarimMagdy96 <karimmagdy096@gmail.com>
2024-08-13 00:08:10 +0300

    up

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,9 +1,23 @@
 import { Link } from "react-router-dom";
 import CartLayout from "./CartLayout";
+import { GiHandTruck } from "react-icons/gi";
+import { useContext } from "react";
+import { cartContext } from "../contexts/CartContext";
 const Cart = () => {
+  const { itemAmount } = useContext(cartContext);
   return (
     <div className=" container mx-auto h-screen   pt-28">
-      <CartLayout />
+      {itemAmount > 0 ? (
+        <CartLayout />
+      ) : (
+        <div className=" flex flex-col justify-center items-center gap-y-4 h-2/3">
+          <GiHandTruck className=" text-[150px] text-primary" />
+
+          <h2 className=" font-medium capitalize ">
+            your cart is empty add product to proceed{" "}
+          </h2>
+        </div>
+      )}
       <div className="  flex flex-col ">
         <Link
           className=" text-white font-semibold text-center bg-black p-3  mb-3 "
--- src/components/Slidebar.tsx
+++ src/components/Slidebar.tsx
@@ -1,4 +1,5 @@
 import { useContext } from "react";
+import { GiHandTruck } from "react-icons/gi";
 
 import { IoMdArrowForward } from "react-icons/io";
 import { Link } from "react-router-dom";
@@ -28,7 +29,17 @@ const Slidebar = () => {
           <IoMdArrowForward className=" text-2xl" />
         </div>
       </div>
-      <CartLayout />
+      {itemAmount > 0 ? (
+        <CartLayout />
+      ) : (
+        <div className=" flex flex-col justify-center items-center gap-y-4 h-2/3">
+          <GiHandTruck className=" text-[150px] text-primary" />
+
+          <h2 className=" font-medium capitalize ">
+            your cart is empty add product to proceed{" "}
+          </h2>
+        </div>
+      )}
       <div className="  flex flex-col ">
         <Link
           onClick={() => SetIsOpen(false)}
