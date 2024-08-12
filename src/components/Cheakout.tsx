0305bf50a9397290081062004ca25f2fb509d2e1
KarimMagdy96 <karimmagdy096@gmail.com>
2024-08-12 21:58:40 +0300

    up

--- src/components/Cheakout.tsx
+++ src/components/Cheakout.tsx
@@ -1,4 +1,3 @@
-import { useState, useEffect } from "react";
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 import CheckoutForm from "./CheckoutForm";
@@ -6,37 +5,15 @@ import CheckoutForm from "./CheckoutForm";
 const stripePromise = loadStripe(import.meta.env.VITE_STRIPE_PUBLIC_KEY);
 
 const Checkout = () => {
-  const [clientSecret, setClientSecret] = useState<string | null>(null);
-
-  useEffect(() => {
-    // Fetch the clientSecret from your serverless function
-    fetch("/api/create-payment-intent", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        currency: "usd",
-        amount: 100, // Amount in cents
-      }),
-    })
-      .then((res) => res.json())
-      .then((data) => setClientSecret(data.client_secret))
-      .catch((error) => console.error("Error fetching client secret:", error));
-  }, []);
-
-  const options = clientSecret
-    ? {
-        clientSecret, // Pass the client secret received from your serverless function
-      }
-    : undefined;
+  const options = {
+    // passing the client secret obtained from the server
+    clientSecret: "{{CLIENT_SECRET}}",
+  };
 
   return (
-    <>
-      <Elements stripe={stripePromise} options={options}>
-        <CheckoutForm />
-      </Elements>
-    </>
+    <Elements stripe={stripePromise} options={options}>
+      <CheckoutForm />
+    </Elements>
   );
 };
 
