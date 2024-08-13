0b3342d39604126ab62073ff671674fcafa34ef0
KarimMagdy96 <karimmagdy096@gmail.com>
2024-08-13 03:02:54 +0300

    up

--- src/components/SignInPage.tsx
+++ src/components/SignInPage.tsx
@@ -13,7 +13,7 @@ const SignInPage = () => {
 
       <section className="bg-white  overflow-hidden ">
         <div className="lg:grid  lg:min-h-screen lg:grid-cols-12">
-          <section className=" pt-40  relative mt-20 flex justify-center items-center  h-32 items-end bg-gray-900 lg:col-span-5 lg:h-full xl:col-span-6">
+          <section className="  relative  flex justify-center items-center  h-32 items-end bg-gray-900 lg:col-span-5 lg:h-full xl:col-span-6">
             <div className="bg-hero bg-cover bg-right absolute  inset-0 h-full w-full object-cover opacity-80"></div>
 
             <div className="hidden b  lg:relative lg:block lg:p-12">
